123cea09b1d4f180007a2f60b06b868d827a3fbc
norgeous <daniel@harte.me>
2022-08-29 14:43:20 +0100

    add react

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  (
+    <h1>react app!</h1>
+  ),
+  rootElement,
+);
