5b212092c19787bd0d0edac34817f7beb28ac25b
norgeous <daniel@harte.me>
2022-08-29 17:43:00 +0100

    move info to readme

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 ## demo
 
 https://norgeous.github.io/gh-pages-pwa
+
+
+## features
+
+- PWA with offline mode
+- Zero install
+- Zero build
+- Clientside es modules (via importmap + skypack cdn)
+- Clientside React transpilation (via babel-standalone within service worker)
+- styled-components
+- less css compiler
+- Emoji can be used for favicon and PWA icon
--- src/components/App.js
+++ src/components/App.js
@@ -9,16 +9,6 @@ const App = () => (
   <>
     <Icon>⚛️📲</Icon>
     <h1>React app!</h1>
-    <ul>
-      <li>PWA with offline mode</li>
-      <li>Zero install</li>
-      <li>Zero build</li>
-      <li>Clientside es modules (via importmap + skypack cdn)</li>
-      <li>Clientside React transpilation (via babel-standalone within service worker)</li>
-      <li>less css compiler</li>
-      <li>styled-components</li>
-      <li>Emoji can be used for favicon and PWA icon</li>
-    </ul>
   </>
 );
 
