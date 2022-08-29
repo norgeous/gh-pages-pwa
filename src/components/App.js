82f9f78edad9be5b2040b42f18f3a7daa9ecbddc
norgeous <daniel@harte.me>
2022-08-29 18:05:28 +0100

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-const Icon = styled.div`
-  font-size: 80px;
+const Heading = styled.div`
+  font-size: 30px;
+`;
+
+const A = styled.a`
+  font-size: 20px;
+  :visited,
+  :hover {
+    color: #077;
+  }
 `;
 
 const App = () => (
   <>
-    <Icon>⚛️📲</Icon>
-    <h1>React app!</h1>
-    <a href="https://github.com/norgeous/gh-pages-pwa/" target="_blank">github</a>
+    <img src="icon.svg" width="80" />
+    <Heading>React app!</Heading>
+    <A href="https://github.com/norgeous/gh-pages-pwa/" target="_blank">norgeous/gh-pages-pwa</A>
   </>
 );
 
