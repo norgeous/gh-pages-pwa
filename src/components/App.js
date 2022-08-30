1f5a376bdcd5dc6e57ea9886791fc65deb5fa6e9
norgeous <daniel@harte.me>
2022-08-30 18:12:29 +0100

    toggle info with gear icon

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import usePhaser from '../phaser/usePhaser';
 import { TopLeft, TopRight, BottomRight, BottomLeft, Overlay } from './styled';
@@ -17,21 +17,24 @@ const A = styled.a`
 `;
 
 const App = () => {
+  const [open, setOpen] = useState(false);
   const { game } = usePhaser();
   // console.log(game);
   return (
     <>
-      <Overlay>
-        <img src="icon.svg" width="80" />
-        <Heading>React app!</Heading>
-        <A href="https://github.com/norgeous/gh-pages-pwa/" target="_blank">
-          norgeous/gh-pages-pwa
-        </A>
-      </Overlay>
+      {open && (
+        <Overlay>
+          <img src="icon.svg" width="80" />
+          <Heading>React app!</Heading>
+          <A href="https://github.com/norgeous/gh-pages-pwa/" target="_blank">
+            norgeous/gh-pages-pwa
+          </A>
+        </Overlay>
+      )}
       <TopLeft>1,000,000</TopLeft>
-      <TopRight><button>⚙️</button></TopRight>
+      <TopRight><button onClick={() => setOpen(!open)}>⚙️</button></TopRight>
       <BottomRight>🪙x22</BottomRight>
-      <BottomLeft>❤️❤️❤️🖤🖤</BottomLeft> 
+      <BottomLeft>❤️❤️🖤</BottomLeft> 
       <div id="phaser"></div>
     </>
   );
